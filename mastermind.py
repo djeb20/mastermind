0c37d0ee109ac494d013f561a50803e384c54894
Dan <danbeechey99@icloud.com>
2022-06-28 13:09:05 +0100

    bugs

--- mastermind.py
+++ mastermind.py
@@ -23,23 +23,35 @@ class mastermind:
 
         self.test_goal = np.arange(4) + 1
 
+        self.width = 4
+        self.height = 12
+
+        # Trying to speed up learning
+        self.count = 0
+
+        # To make super fast, transitions dictionary.
+        self.trans = TransDict()
+        self.trans.height = self.height
+        self.trans.width = self.width
+
     def reset(self):
         """
         Resets environment to empty grid with new goal.
         """
 
-        self.grid = np.zeros((12, 6))
-        self.grid_render = np.full((12, 6), ' ', dtype='O')
         self.count = 0
 
-        while True:
-            goal = np.random.randint(0, 8, 4) + 1
-            if not (goal == self.test_goal).all():
-                break
+        self.grid = np.zeros((self.height, self.width + 2))
 
-        self.goal = goal
-        # self.goal = np.array([0, 1, 2, 3]) + 1
-        self.goal_render = np.array([self.colour_dict[i - 1] for i in self.goal])
+        if self.count % (100 * self.height) == 0:
+
+            while True:
+                goal = np.random.randint(0, 8, 4) + 1
+                if not (goal == self.test_goal).all():
+                    break
+
+            self.goal = goal
+            self.goal_render = np.array([self.colour_dict[i - 1] for i in self.goal])
 
         return self.grid.flatten()
     
@@ -48,33 +60,48 @@ class mastermind:
         Step in the environment, places piece in next available square.
         """
 
+        self.grid, reward, done = self.trans[tuple([self.grid.tobytes(), action, self.goal.tobytes(), self.count])]
+
+        # if tuple([self.grid.tobytes(), action, self.goal.tobytes(), self.count]) not in self.trans:
+
+        #     self.grid, reward, done = self.take_step(action, self.grid, self.goal, self.count)
+        #     self.trans[tuple([self.grid.tobytes(), action, self.goal.tobytes(), self.count])] = (self.grid, reward, done)
+
+        # else:
+
+        #     self.grid, reward, done = self.trans[tuple([self.grid.tobytes(), action, self.goal.tobytes(), self.count])]
+
+        self.count += 1
+
+        return self.grid.flatten(), reward, done, False
+
+    def take_step(self, action, grid, goal, count):
+        """
+        Calculates reward etc for step in environment.
+        """
+
         # Negative reward for each step
         reward = -1
         done = False
 
-        row = self.count // 4
-        col = self.count % 4
+        row = count // 4
+        col = count % 4
 
-        self.grid[row, col + 1] = action + 1
-        self.grid_render[row, col + 1] = self.colour_dict[action]
+        grid[row, col + 1] = action + 1
 
         if (col - 3) == 0: # row is complete
 
             # Number exactly right
-            right_ind = self.grid[row][1:5] == self.goal
+            right_ind = grid[row][1:5] == goal
             right = (right_ind).sum()
 
             # Number that are close, need more efficient solution
-            d = dict(zip(*np.unique(self.grid[row][1:5][right_ind], return_counts=True)))
-            sum = np.sum([d[colour] for colour in self.goal if colour in d])
+            d = dict(zip(*np.unique(grid[row][1:5][right_ind], return_counts=True)))
+            sum = np.sum([d[colour] for colour in goal if colour in d])
             close = right - sum
 
-            self.grid[row][0] = close
-            self.grid[row][-1] = right
-
-            # Just for rendering
-            self.grid_render[row][0] = close
-            self.grid_render[row][-1] = right
+            grid[row][0] = close
+            grid[row][-1] = right
 
             if row == 11: # Finished game with no win
                 done = True
@@ -83,15 +110,41 @@ class mastermind:
                 done = True
                 reward += 30
 
-        self.count += 1
-
-        return self.grid.flatten(), reward, done, False
+        return grid, reward, done
 
     def render(self):
+        """
+        Render environment, expensive!
+        Could map be used?
+        """
+
+        self.grid_render = np.full((12, 6), ' ')
+
+
+        for i, in range(self.height):
+            for j in range(1, self.width - 1):
+
+                self.grid_render[i, j] = self.colour_dict[self.grid[i, j]]
 
         print(np.flipud(self.grid_render))
 
+class TransDict(dict, mastermind):
+    
+    def __missing__(self, key):
+
+        grid, action, goal, count = key
+        val = self.take_step(np.frombuffer(grid, dtype="int64").reshape(self.height, self.width), 
+                             action, 
+                             np.frombuffer(goal, dtype="int64"), 
+                             count)
+        # val = self.take_step(np.frombuffer(grid, dtype="int32").reshape(self.height, self.width), 
+        #                      action, 
+        #                      np.frombuffer(goal, dtype="int32"), 
+        #                      count)
         
+        self.__setitem__(key, val)
+        
+        return val
 
 
 
