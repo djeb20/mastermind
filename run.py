e0304dbd0c0b565841ac43f051fe2213420af68a
djeb20 <danbeechey99@icloud.com>
2023-04-02 17:48:21 +0100

    new agents

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ from mastermind import mastermind
 
 env = mastermind(action_type='guess', reward_struc='basic', num_goals=10)
 
-from dqn import Agent
-agent = Agent(env, state_dim=env.state_dim, num_actions=env.action_dim,
-                 critic_arch=[128, 64], buffer_size=1e6, batch_size=64, decay=0.999,
-                 gamma=0.99, step_size=1e-4, 
-                 tau=0.001, epsilon_start=1, epsilon_min=0.05)
+# from dqn import Agent
+# agent = Agent(env, state_dim=env.state_dim, num_actions=env.action_dim,
+#                  critic_arch=[128, 64], buffer_size=1e6, batch_size=64, decay=0.999,
+#                  gamma=0.99, step_size=1e-4, 
+#                  tau=0.001, epsilon_start=1, epsilon_min=0.05)
 
-agent.fill_buffer(exp=True, multi=False, prop=1)
-agent.train(1e7)
+# agent.fill_buffer(exp=True, multi=False, prop=1)
+# agent.train(1e7)
 
 from ppo_agent import Agent
 agent = Agent(env, state_dim=env.state_dim, num_actions=env.action_dim,
